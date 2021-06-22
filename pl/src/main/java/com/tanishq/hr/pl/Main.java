4bd2cf3e43e8a0824f765e7aabf51bcff5a1afd6
Tanishq Rawat <67191912+tanishq-17102001@users.noreply.github.com>
2021-06-22 15:38:33 +0530

    Create Main.java

---
+++ pl/src/main/java/com/tanishq/hr/pl/Main.java
@@ -0,0 +1,10 @@
+package com.tanishq.hr.pl;
+import com.tanishq.hr.pl.ui.*;
+class Main
+{
+public static void main(String args[])
+{
+DesignationUI designationUI=new DesignationUI();
+designationUI.setVisible(true);
+}
+}
