8cd68cb876837c24df3087b7286b76cc01a460a6
Tanishq Rawat <67191912+tanishq-17102001@users.noreply.github.com>
2021-06-22 15:33:32 +0530

    Create DesignationDTOInterface.java

---
+++ dl/src/com/tanishq/hr/dl/interfaces/dto/DesignationDTOInterface.java
@@ -0,0 +1,8 @@
+package com.tanishq.hr.dl.interfaces.dto;
+public interface DesignationDTOInterface extends Comparable<DesignationDTOInterface>,java.io.Serializable
+{
+public void setCode(int code);
+public int getCode();
+public void setTitle(String title);
+public String getTitle();
+}
