05510fa254d40e8aa0f3c76a4371121fb101a262
Tanishq Rawat <67191912+tanishq-17102001@users.noreply.github.com>
2021-06-22 15:16:31 +0530

    Create DesignationInterface.java

---
+++ bl/src/main/java/com/tanishq/hr/bl/interfaces/pojo/DesignationInterface.java
@@ -0,0 +1,8 @@
+package com.tanishq.hr.bl.interfaces.pojo;
+public interface DesignationInterface extends java.io.Serializable,Comparable<DesignationInterface>
+{
+public void setCode(int code);
+public void setTitle(String title);
+public int getCode();
+public String getTitle();
+}
