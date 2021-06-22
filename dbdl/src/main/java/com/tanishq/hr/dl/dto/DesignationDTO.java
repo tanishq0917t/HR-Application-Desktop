36cb529ea268547ed1e1a88be2257ed25a77748d
Tanishq Rawat <67191912+tanishq-17102001@users.noreply.github.com>
2021-06-22 15:46:40 +0530

    Create DesignationDTO.java

---
+++ dbdl/src/main/java/com/tanishq/hr/dl/dto/DesignationDTO.java
@@ -0,0 +1,44 @@
+package com.tanishq.hr.dl.dto;
+import com.tanishq.hr.dl.interfaces.dto.*;
+public class DesignationDTO implements DesignationDTOInterface
+{
+private int code;
+private String title;
+public DesignationDTO()
+{
+this.code=0;
+this.title="";
+}
+public void setCode(int code)
+{
+this.code=code;
+}
+public int getCode()
+{
+return this.code;
+}
+public void setTitle(String title)
+{
+this.title=title;
+}
+public String getTitle()
+{
+return this.title;
+}
+public boolean equals(Object other)
+{
+if(!(other instanceof DesignationDTOInterface)) return false;
+DesignationDTOInterface designationDTO;
+designationDTO=(DesignationDTO)other;
+return this.code==designationDTO.getCode();
+}
+public int compareTo(DesignationDTOInterface designationDTO)
+{
+return this.code-designationDTO.getCode();
+}
+public int hashCode()
+{
+return code;
+}
+
+}
